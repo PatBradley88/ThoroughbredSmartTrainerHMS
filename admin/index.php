431b3444c4e0038f7d27ea210a8f6fd0994e9868
Patrick Bradley <patrick_bradley1@yahoo.co.uk>
2020-04-21 22:55:13 +0100

    animated cards added to admin index. Possibly to be used to display stats.

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,58 @@
 
 
             <h1 class="pageHeadingBig">Copper Beech Stables Admin </h1>
+
+            <div class="widgetContainer">
+            	<div class="card">
+            		<div class="face face1">
+            			<div class="widgetContent">
+            				<img src="../images/bet.png">
+            				<h3>Horses</h3>
+            			</div>
+            		</div>
+            		<div class="face face2">
+            			<div class="widgetContent">
+            				<p>Horse in training at 
+            				Copper Beech Stables.</p>
+            				<a href="#">Horses</a>
+            			</div>
+            		</div>
+            	</div>
+            	<div class="card">
+            		<div class="face face1">
+            			<div class="widgetContent">
+            				<img src="../images/horse.png">
+            				<h3>Owners</h3>
+            			</div>
+            		</div>
+            		<div class="face face2">
+            			<div class="widgetContent">
+            				<p>Horse in training at 
+            				Copper Beech Stables.</p>
+            				<a href="#">Owners</a>
+            			</div>
+            		</div>
+            	</div>
+            	<div class="card">
+            		<div class="face face1">
+            			<div class="widgetContent">
+            				<img src="../images/bet.png">
+            				<h3>Other</h3>
+            			</div>
+            		</div>
+            		<div class="face face2">
+            			<div class="widgetContent">
+            				<p>Horse in training at 
+            				Copper Beech Stables.</p>
+            				<a href="#">Other</a>
+            			</div>
+            		</div>
+            	</div>
+            </div>    
+
+
+
+
         </div>
     </div>
                         
