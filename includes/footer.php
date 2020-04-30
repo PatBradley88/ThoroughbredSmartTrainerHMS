05750ef59cae6482767d78bc5387d31dfec336c7
Patrick Bradley <patrick_bradley1@yahoo.co.uk>
2020-04-30 22:41:12 +0100

    unused files deleted.

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,4 @@
-<!-- Footer -->
-        <!-- <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Thoroughbred Smart Trainer 2020</p>
-                </div>
-                <!-- /.col-lg-12 -->
-           <!--  </div> -->
-            <!-- /.row -->
-        <!-- </footer> -->
+
 
     </div>
     <!-- /.container -->
